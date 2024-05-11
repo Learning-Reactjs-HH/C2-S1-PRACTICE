768f8979de50a2e67c0f6164da6de0f18e5acc18
Hoàng <hoanghung20030@gmail.com>
2024-05-11 11:03:36 +0700

    exercise 3

--- Start code/exercise-3/src/components/Time.jsx
+++ Start code/exercise-3/src/components/Time.jsx
@@ -1,8 +1,23 @@
 function Time() {
-    let date = new Date().toLocaleString() + ""
-    return(
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    let amOrPm = hours >= 12 ? 'PM' : 'AM';
+
+    // Chuyển đổi giờ sang định dạng 12 giờ
+    hours = hours % 12 || 12;
+
+    // Thêm số 0 trước nếu giờ, phút hoặc giây nhỏ hơn 10
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    let time = hours + ":" + minutes + ":" + seconds + " " + amOrPm;
+
+    return (
         <h2>
-            {date}
+            {time}
         </h2>
     );
 }
